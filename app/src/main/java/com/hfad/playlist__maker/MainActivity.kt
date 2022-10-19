8e352231ab044cd7423c7a7e1baacdd6fec4df1b
johnwaynejr <navuhodonosor11@gmail.com>
2022-10-19 23:19:35 +0300

    Добавил обработчик кнопки iV_Share. Убрал лишние комментарии.

--- app/src/main/java/com/hfad/playlist__maker/MainActivity.kt
+++ app/src/main/java/com/hfad/playlist__maker/MainActivity.kt
@@ -13,29 +13,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnFindId = findViewById<Button>(R.id.btnFind)
-        /*val btnFindListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку поиск!", Toast.LENGTH_SHORT).show()
-                btnFindId.setOnClickListener(btnFindListener)
-            }*/
-            btnFindId.setOnClickListener {
+           btnFindId.setOnClickListener {
                 val displayIntent = Intent(this, FindActivity::class.java)
                 startActivity(displayIntent)
             }
 
         val btnMediaId = findViewById<Button>(R.id.btnMedia)
-       /* btnMediaId.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека!", Toast.LENGTH_SHORT).show()
-        }*/
         btnMediaId.setOnClickListener {
             val displayIntent = Intent(this, MedialibraryActivity::class.java)
             startActivity(displayIntent)
         }
 
         val btnSettingsId = findViewById<Button>(R.id.btnSettings)
-        /*btnSettingsId.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки!", Toast.LENGTH_SHORT).show()
-        }*/
         btnSettingsId.setOnClickListener {
             val displayIntent = Intent(this, SettigsActivity::class.java)
             startActivity(displayIntent)
--- app/src/main/java/com/hfad/playlist__maker/SettigsActivity.kt
+++ app/src/main/java/com/hfad/playlist__maker/SettigsActivity.kt
@@ -1,11 +1,23 @@
 package com.hfad.playlist___maker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettigsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val btnShareId = findViewById<ImageView>(R.id.iV_Share)
+        btnShareId.setOnClickListener {
+            val sendIntent = Intent()
+            sendIntent.action = Intent.ACTION_SEND
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/")
+            sendIntent.type = "text/plain"
+            startActivity(sendIntent)
+        }
+
     }
 }
