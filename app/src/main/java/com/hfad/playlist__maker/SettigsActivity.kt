fbd3bd8a4ea38b896bc5b578f0dc1422a370ee2a
johnwaynejr <navuhodonosor11@gmail.com>
2022-10-22 14:39:23 +0300

    Добавил обработчик кнопок окна "Настройки"

--- app/src/main/java/com/hfad/playlist__maker/SettigsActivity.kt
+++ app/src/main/java/com/hfad/playlist__maker/SettigsActivity.kt
@@ -1,6 +1,7 @@
 package com.hfad.playlist___maker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ class SettigsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        //Обработка нажатия на кнопку "Поделиться приложением"
         val btnShareId = findViewById<ImageView>(R.id.iV_Share)
         btnShareId.setOnClickListener {
             val sendIntent = Intent()
@@ -18,6 +20,26 @@ class SettigsActivity : AppCompatActivity() {
             sendIntent.type = "text/plain"
             startActivity(sendIntent)
         }
+        //Обработка нажатия на кнопку "Написать в поддержку"
+        val btnSupportId = findViewById<ImageView>(R.id.iV_Support)
+        btnShareId.setOnClickListener{
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse("Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("for_mail_box@mail.ru"))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(supportIntent)
+        }
+
+        //Обработка нажатия на кнопку "Написать в поддержку"
+        val btnUserAgreementId = findViewById<ImageView>(R.id.iV_Support)
+        btnUserAgreementId.setOnClickListener{
+            val url = "http://www.example.com";
+            val agreementIntent = Intent(Intent.ACTION_VIEW);
+            agreementIntent.setData(Uri.parse(url));
+            startActivity(agreementIntent)
+        }
+
 
     }
 }
