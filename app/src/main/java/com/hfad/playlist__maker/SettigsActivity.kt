62e5737dfdbab0d1a51aceda9d9ee295f0096381
johnwaynejr <navuhodonosor11@gmail.com>
2022-10-22 15:39:05 +0300

    Исправлены ошибки

--- app/src/main/java/com/hfad/playlist__maker/SettigsActivity.kt
+++ app/src/main/java/com/hfad/playlist__maker/SettigsActivity.kt
@@ -14,25 +14,26 @@ class SettigsActivity : AppCompatActivity() {
         //Обработка нажатия на кнопку "Поделиться приложением"
         val btnShareId = findViewById<ImageView>(R.id.iV_Share)
         btnShareId.setOnClickListener {
-            val sendIntent = Intent()
-            sendIntent.action = Intent.ACTION_SEND
+            val sendIntent = Intent(Intent.ACTION_SEND)
             sendIntent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/")
             sendIntent.type = "text/plain"
             startActivity(sendIntent)
         }
         //Обработка нажатия на кнопку "Написать в поддержку"
         val btnSupportId = findViewById<ImageView>(R.id.iV_Support)
-        btnShareId.setOnClickListener{
+        btnSupportId.setOnClickListener{
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
             val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
             val supportIntent = Intent(Intent.ACTION_SENDTO)
-            supportIntent.data = Uri.parse("Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            supportIntent.data = Uri.parse("mailto:")
             supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("for_mail_box@mail.ru"))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
             supportIntent.putExtra(Intent.EXTRA_TEXT, message)
             startActivity(supportIntent)
         }
 
         //Обработка нажатия на кнопку "Написать в поддержку"
-        val btnUserAgreementId = findViewById<ImageView>(R.id.iV_Support)
+        val btnUserAgreementId = findViewById<ImageView>(R.id.iV_UserAgreement)
         btnUserAgreementId.setOnClickListener{
             val url = "https://yandex.ru/legal/practicum_offer/";
             val agreementIntent = Intent(Intent.ACTION_VIEW);
