a2eef4893793a91ffd6dbcaaf79024d2e8692efd
johnwaynejr <navuhodonosor11@gmail.com>
2022-10-26 22:22:35 +0300

    Добавил TextWatcher и обработку нажатия кнопки очистки поиска

--- app/src/main/java/com/hfad/playlist__maker/FindActivity.kt
+++ app/src/main/java/com/hfad/playlist__maker/FindActivity.kt
@@ -1,11 +1,50 @@
 package com.hfad.playlist___maker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class FindActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find)
-    }
+
+        val inputEditText = findViewById<EditText>(R.id.et_find)
+        val clearButton = findViewById<Button>(R.id.btn_clear)
+        clearButton.visibility= View.INVISIBLE
+
+
+        clearButton.setOnClickListener {
+            inputEditText.text.clear()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+        }
+        val simpleTextWatcher = object:TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
+                if (s.isNullOrEmpty()) {
+                    clearButton.visibility= View.INVISIBLE
+                } else {
+                    clearButton.visibility= View.VISIBLE
+                }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //empty
+            }
+        }
+
+        //добавляем созданный simpleTextWatcher к EditText
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+        }
+
 }
